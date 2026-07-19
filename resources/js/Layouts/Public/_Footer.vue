11f70832ff6fce0830cf2370a9ed944dde5adb40
Nima <115721605+nniiiimmaa@users.noreply.github.com>
2026-07-19 11:37:22 -0300

    Comment out social media links in _Footer.vue
    Commented out the social media links rendering code in the footer.

--- resources/js/Layouts/Public/_Footer.vue
+++ resources/js/Layouts/Public/_Footer.vue
@@ -6,9 +6,9 @@
                 <NavLogo />
             </Link>
             <div class="footer-socials">
-                <a v-for="social in socialLinks" :key="social.name" :href="social.url" target="_blank"
+               <!-- <a v-for="social in socialLinks" :key="social.name" :href="social.url" target="_blank"
                     rel="noopener noreferrer" class="social-icon" :aria-label="social.name">
-                    <img :src="social.icon" :alt="social.name" />
+                    <img :src="social.icon" :alt="social.name" /> -->
                 </a>
             </div>
         </div>
